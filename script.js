ace8bcb993457d109cc6b98b97eee8f8470d58b7
Nazmul Hussain <typetonazmul@gmail.com>
2021-05-05 22:52:45 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('#btn').addEventListener('click', function() {
+	if (navigator.share) {
+		navigator.share({
+			title: 'my title',
+			url: 'my url'
+		}).then(() => {
+			console.log('thanks for sharing')
+		})
+		.catch(console.error)
+	}
+	else {
+		console.log('not mobile')
+	}
+})
