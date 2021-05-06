1f8d6e7195482cadc5b34c8ce1a568d426d2fe7d
Nazmul Hussain <typetonazmul@gmail.com>
2021-05-06 19:51:25 +0600

    updated

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 document.querySelector('#btn').addEventListener('click', function() {
 	if (navigator.share) {
 		navigator.share({
-			title: 'my title',
+			title: 'Title 1',
 			url: ''
-		}).then(() => {
-			console.log('thanks for sharing')
 		})
-		.catch(console.error)
 	}
 	else {
-		console.log('not mobile')
+		alert('not supported')
+	}
+})
+
+
+document.querySelector('#btn2').addEventListener('click', function() {
+	if (navigator.share) {
+		navigator.share({
+			title: 'Title 2',
+			url: 'sub-folder'
+		})
+	}
+	else {
+		alert('not supported')
+	}
+})
+
+
+document.querySelector('#btn3').addEventListener('click', function() {
+	if (navigator.share) {
+		navigator.share({
+			title: 'Title 3',
+			url: 'sub-folder/sub-folder/index.html'
+		})
+	}
+	else {
+		alert('not supported')
 	}
 })
